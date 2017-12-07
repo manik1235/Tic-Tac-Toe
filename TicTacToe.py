c92a8916ea0f250f620d9ad512a0039c64f1634f
Andrew Henning-Kolberg <andrew.kolberg@gmail.com>
2017-12-06 23:11:03 -0500

    Replaced checkBoard function with a list of list, based on suggestion by /u/thefryscorer

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 '''
 A simple Tic-Tac-Toe Game in Python 3
 '''
@@ -7,6 +8,14 @@ import os
 theBoard = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
             'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
             'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
+winConditions = [['top-L','top-M','top-R'],
+                 ['mid-L','mid-M','mid-R'],
+                 ['low-L','low-M','low-R'],
+                 ['top-L','mid-L','low-L'],
+                 ['top-M','mid-M','low-M'],
+                 ['top-R','mid-R','low-R'],
+                 ['top-L','mid-M','low-R'],
+                 ['low-L','mid-M','top-R']]
 def printBoard(board):
     print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
     print('-+-+-')
@@ -16,6 +25,13 @@ def printBoard(board):
 def end(move):
     if move == 'quit':
         raise SystemExit
+def checkBoard2(board):
+    for winCond in winConditions:
+        if (theBoard[winCond[0]] != ' ') and (theBoard[winCond[0]] == theBoard[winCond[1]] == theBoard[winCond[2]]):
+            print('{} is the winner!'.format(theBoard[winCond[0]]))
+            input('Press <Enter> to quit.')
+            raise SystemExit
+
 def checkBoard(board):
     if (board['top-L'] == 'X' and board['mid-L'] == 'X' and board['low-L'] == 'X'):
         print('X is winner!')
@@ -69,23 +85,27 @@ def checkBoard(board):
 turn = 'X'
 move = ' '
 
+print('\nGood luck!\n')
 while move != 'quit':
-    os.system('cls')
     print('Type in "quit" to end game.')
     print('\nTo choose a position, type in which row(top, mid, low) followed by which column(-R, -M, -L')
     print('To move to the top left corner, you would type "top-L", bottom mid would be "low-M", without the quotes.')
     printBoard(theBoard)
-    checkBoard(theBoard)
+    checkBoard2(theBoard)
     print('Turn for ' + turn + '. Move on which space?')
     move = input()
     end(move)
     if (move not in theBoard):
-        print('\nTry again.')
+        os.system('cls')
+        print('\nTry again.\n')
         continue
     elif (theBoard[move] == ' '):
         theBoard[move] = turn
+        os.system('cls')
+        print('\nGood luck!\n')
     else:
-        print('\nTry again.')
+        os.system('cls')
+        print('\nTry again.\n')
         continue
     if turn == 'X':
         turn = 'O'
